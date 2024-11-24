748b09086bc48a409f4d6b1efbac988601fc9be6
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-11-24 13:27:36 +0600

    [FEAT] - Array and Tuples in typescript

---
+++ module-1/src/array.ts
@@ -0,0 +1,9 @@
+// array & tuples
+
+const person = ["John", "Doe", 30, true];
+console.log("Person", person);
+
+let friends: [string, number] = ["John", 30];
+// friends[0] = 1; // Error - Type '1' is not assignable to type 'string'
+// friends[1] = "Doe"; // Error - Type '"Doe"' is not assignable to type 'number'
+console.log("Friends", friends);
