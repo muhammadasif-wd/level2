7f3e18c51d41eaa3a9132eb1cb58caf416c521d9
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-11-24 13:20:22 +0600

    [FEAT] - basic typescript, explore primitive types

---
+++ module-1/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const course = "Next Level Development By Programming Hero";
+console.log("Course", course);
+const primitiveTypes = [
+    "string",
+    "number",
+    "boolean",
+    "null",
+    "undefined",
+    "symbol",
+];
+// example
+const str = "Hello World";
+const num = 42;
+const bool = true;
+const nul = null;
+const undef = undefined;
+const sym = Symbol("symbol");
+console.log("Primitive Types", primitiveTypes);
---
+++ module-1/src/index.ts
@@ -0,0 +1,20 @@
+const course: string = "Next Level Development By Programming Hero";
+console.log("Course", course);
+
+const primitiveTypes: Array<string> = [
+  "string",
+  "number",
+  "boolean",
+  "null",
+  "undefined",
+  "symbol",
+];
+// example
+const str: string = "Hello World";
+const num: number = 42;
+const bool: boolean = true;
+const nul: null = null;
+const undef: undefined = undefined;
+const sym: symbol = Symbol("symbol");
+
+console.log("Primitive Types", primitiveTypes);
